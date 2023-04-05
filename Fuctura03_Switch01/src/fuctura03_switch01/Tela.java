fbc523f43b634bd9d5f83e9b3161b39e13479e66
Suelen Feijo <78566963+Suelenfm@users.noreply.github.com>
2023-04-05 05:24:54 -0300

    apaguei comentários

--- Fuctura03_Switch01/src/fuctura03_switch01/Tela.java
+++ Fuctura03_Switch01/src/fuctura03_switch01/Tela.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package fuctura03_switch01;
 
-/**
- *
- * @author DELL
- */
+ 
 public class Tela {
     public static void main(String[] args) {
         Pessoa p1= new Pessoa ();
