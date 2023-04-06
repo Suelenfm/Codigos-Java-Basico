06cb7a6ea1a7b62f6f6c58cc1da8479f7d480958
Suelen Feijo <78566963+Suelenfm@users.noreply.github.com>
2023-04-06 19:50:31 -0300

    repetir x cambalhotas: do while

---
+++ Repeticao01/src/repeticao01/Repeticao01.java
@@ -0,0 +1,18 @@
+
+package repeticao01;
+
+public class Repeticao01 {
+
+    public static void main(String[] args) {
+        
+        int cc = 0;
+        do {
+            cc++;
+            System.out.println("cambalhota " + cc);
+            
+        }while (cc<4);
+            
+      
+    }
+    
+}
