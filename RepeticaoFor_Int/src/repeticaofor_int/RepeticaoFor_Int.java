2933e78772477e8da077ae11914f93f0dec28d99
Suelen Feijo <78566963+Suelenfm@users.noreply.github.com>
2023-04-06 20:03:56 -0300

    Combinação: for int

---
+++ RepeticaoFor_Int/src/repeticaofor_int/RepeticaoFor_Int.java
@@ -0,0 +1,26 @@
+
+package repeticaofor_int;
+
+public class RepeticaoFor_Int {
+
+    public static void main(String[] args) {
+       
+       
+      /*  for (int cc = 1; cc<=5; cc++){
+        System.out.println("Cambalhota ");
+        */
+      /*for (int cc = 20; cc>=5; cc-=2){
+        System.out.println("cc");*/
+      
+     /* for (int cc = 0; cc<4 || cc==5 ; cc++){
+        System.out.println("Cambalhota ");*/
+     
+     for (int i = 1; i<=4 ; i++){
+         for (int j = 1; j<=4; j++){
+             System.out.println(i +","+ j);
+         }
+       }
+     }
+    
+    
+}
