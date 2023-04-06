46f9e9605aa6e8f0fec2b594d5866f5e16f7ef9d
Suelen Feijo <78566963+Suelenfm@users.noreply.github.com>
2023-04-06 19:38:31 -0300

    Decremento, incremento, Classe Math

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,40 @@
+
+package operadoresaritmeticos;
+
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+      /* int n1 = 3;
+        int n2 = 5;
+        float m = (n1+n2) /2;
+        System.out.printf("A soma das notas é: %.1f " , m);
+        
+        int numero = 6;
+       
+        //numero++;
+        // numero--;
+        int valor = 10 + ++numero;
+         //int valor = 7 + ++numero;
+         //int valor = 7 + --numero;
+         // int valor = 7 + numero--;
+        System.out.println(numero);
+        System.out.println(valor); */
+        
+        /* int x = 4;
+        x = 2; //x recebe 2 ( x = x + 2)
+        System.out.println(x);*/
+        
+        //float v  = 8.9f;
+        //int ar = (int) Math.floor(v); // = arredonda pra baixo
+        //int ar = (int) Math.ceil(v);  //arredonda pra cima
+        /*int ar = (int) Math.round(v);  //arredonda pra cima
+        System.out.println(ar);*/
+        
+        double ale = Math. random(); //gera número entre 0 e 1
+        int n = (int)(5 + ale *  (20-5)); 
+        //System.out.println(ale);
+        System.out.println(n);
+        }
+    
+    
+}
